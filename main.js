6623f67a1727481f1a6dde668a8dbf88496c8d5f
Eduardo <rotjeot@gmail.com>
2023-09-22 00:03:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let sound = new Audio('cancion.mp3');
+let flg = false 
+BtnPlay.addEventListener('click',()=>{
+    flg = !flg
+    if(flg){
+        sound.play();
+    }
+    else{
+        sound.pause();
+    }
+s
+});
